3b1e93b586fb8c87d5d3bb793f6f56fc55667b16
henriw22 <henriw22@gmail.com>
2021-06-06 23:00:52 +0000

    finished the code based on the required features

---
+++ timer2.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'BEEP> '
+});
+
+rl.prompt();
+
+
+rl.on('line', (line) => {
+  if (isNaN(line) && line === 'b') {
+    console.log('test');
+  }
+  if (!isNaN(line)) {
+    console.log(`"setting timer for ${line} seconds..."`);
+    setTimeout(() => {
+      process.stdout.write('\x07')
+    }, line * 1000);
+  }
+  rl.prompt();
+})
+
+.on('close', () => {
+  console.log('Thanks for using me, ciao!');
+  process.exit(0);
+});
+
+
+// let time = 0;
+// if (time.length !== 0) {
+//   for (let x of time) {
+//     if (!(Number.isNaN(x) || x < 0)) {
+//       setTimeout(() => {
+//         process.stdout.write('\x07')
+//       }, x);
+//     }
+//   }
+// }
